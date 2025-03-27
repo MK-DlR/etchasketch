c09b98a931a3fced1565b8449a7d4d640e9e970a
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-03-27 14:00:26 -0400

    Replace etchasketch.js with etchasketchJS.js Add first step of project to etchasketchJS

--- etchasketchJS.js
+++ etchasketchJS.js
@@ -1,10 +1,10 @@
-/* etchasketchJS.js */
+/* etchasketchJS.js */ 
 
 // 1. create a 16x16 grid of square divs using JS and Flexbox
     // put grid squares inside a “container” div (div can be written into HTML file)
     // use Flexbox to make divs appear as a grid (versus just one on each line)
     // be careful with borders and margins - they can adjust the size of the squares
-    
+
     // troubleshooting: "why isn’t my grid being created?"
         // is the css stylesheet linked - yes
         // open browser devtools
