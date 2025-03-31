7932eb3ba5d331e34397b31f0959cbcc14ad7c8f
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-03-31 15:05:40 -0400

    Creation of 16 by 16 squares grid
    Adjust gridSquare divs to 50px by 50x with no text content inside
    Move gridSquare creation inside of createGrid function

--- etchasketchJS.js
+++ etchasketchJS.js
@@ -1,53 +1,33 @@
 /* etchasketchJS.js */
 
-/*
-using JS + flexbox:
-create 16x16 grid
-of squares (divs?)
-with a black border
-and no text content
-inside of a container div (created in html file)
-use flexbox:
-to make divs appear as grid
-be careful with borders and margins
-as they can adjust the size of the squares
-*/
-
 // creating container element
-const container = document.querySelector("#container");
-
-document.getElementById("container").style.width = "500px";
-
-// create grid square element
-const gridSquare = document.createElement("div");
-gridSquare.classList.add("gridSquare");
-gridSquare.style.border = "solid";
-gridSquare.style.borderWidth = "thin";
-gridSquare.textContent = "I should become a square";
-
-// add grid square to page
-// container.appendChild(gridSquare);
+const container = document.getElementById("container");
 
 // set grid dimensions
-let width = 16;
-let height = 16;
+let num = 16;
 
-// function with for loop to create grid
-function createGrid(horizontalSize, verticalSize) {
-  let grid = "";
+// for loop function to create grid rows
+function createGridRow(num) {
+  for (let x = 0; x < num; x++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.setAttribute("class", "new");
+    gridSquare.classList.add("gridSquare");
+    // grid square styling
+    gridSquare.style.width = "50px";
+    gridSquare.style.height = "50px";
+    gridSquare.style.border = "solid";
+    gridSquare.style.borderWidth = "thin";
 
-  for (let y = 0; y < verticalSize; y++) {
-    let currentRow = "";
-    for (let x = 0; x < horizontalSize; x++) {
-      currentRow += " X ";
-    }
-    if (y < verticalSize - 1) {
-      grid += currentRow + "\n";
-    } else {
-      grid += currentRow;
-    }
+    container.appendChild(gridSquare);
   }
-  return grid;
 }
 
-console.log(createGrid(width, height)); // currently only showing in console log
+// for loop function to create grid columns by calling grid row function
+function createGridColumn(num) {
+  for (let y = 1; y < num; y++) {
+    createGridRow(num);
+  }
+}
+
+// calling functions to create grid
+createGridColumn(num);
